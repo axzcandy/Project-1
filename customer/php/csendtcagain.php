6cb5db8b3b26b7d56ecbddc65a6d71a1f98d0362
Andy <axzc8901@gmail.com>
2018-04-25 06:17:33 +0800

    temp fix
    still need to verify

--- customer/php/csendtcagain.php
+++ customer/php/csendtcagain.php
@@ -1,33 +1,41 @@
 <?php
 	include('sqlconnstorage.php');
 	
-	if(1){
+	if(checkCustomerLoginValid()==true){
 		// checkCustomerLoginValid()==true
-		$tempcodeforemail = $_SESSION['tcode']; 
+		
 		$email = $_SESSION['email'];
 		
-		$sql = "select statue as st from customeridpass where Email = '$email'";
+		$sql = "select Statue as st,TempCode as tp from customeridpass where Email = '$email'";
 		$result = mysqli_query($conn,$sql);
 		$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
 		$stat = $row['st'];
-
+		$tempcode = $row['tp'];
 		if($stat=='new'){
 			$subject="Email verification code";
+			
+			$tempcodeforemail=$tempcode;
+
 			$message="Your verify code is = ".$tempcodeforemail."\n";
 			$headers="Verify Code";
 						
 			if(mail($email,$subject,$message,$headers)==true){
+				echo $email.$subject.$message.$headers.$stat;
 				echo "Send...Redirecting";
-				header("Refresh: 1;URL= /projectv2/customer/html/cverifyemail.html");
+				header("Refresh: 3;URL= /projectv2/customer/html/cverifyemail.html");
 			}else{
 				// echo 'failed0';
+				echo "'ERROR:mailsenderror' Please contact support!";
 			}
-		}else{
-			echo "Alr verify";
+		}else if($stat=='locked'){
+			echo "Account locked! Contact support for help.";
 			header("Refresh: 1;URL= /projectv2/customer/html/csmainpage.html");
 		}
 	}else{
-		echo "Please login!!";
-		header("Refresh: 1;URL= /projectv2/customer/html/customer register.html");
+		unset($_SESSION['cs']);
+		unset($_SESSION['lastactive']);
+		unset($_SESSION['tcode']);
+		echo "Please login!!!";
+		header("Refresh: 4;URL= /projectv2/customer/html/customerlogin.html");
 	}
 ?>
